47fa8fa3dac7352d455e99efe1655b58746d56f9
Rojina Dhakal <128691590+Rojina0803@users.noreply.github.com>
2024-03-11 18:01:53 +0545

    11mar.js

--- 11mar.js
+++ 11mar.js
@@ -1,68 +1,68 @@
 
 // ARGUEMENT OBJECTS
 
-// function display(name1,name2){
-//     // document.write(name);
-//     arguments[0]="Series";
-//     document.write(arguments[0] + " : ")
-//     document.write(arguments[1])
-// }
-// display("GOT","Games");
+function display(name1,name2){
+    // document.write(name);
+    arguments[0]="Series";
+    document.write(arguments[0] + " : ")
+    document.write(arguments[1])
+}
+display("GOT","Games");
 
-// function length(a,b,cc){
-//     document.write(arguments.length);
-// }
-// length(1,2,3);
+function length(a,b,cc){
+    document.write(arguments.length);
+}
+length(1,2,3);
 
 
-// function args(u,i,o,g){
-//     for(let i=0; i<arguments.length; i++){
-//         document.write(arguments[i] + " :");
-//     }
+function args(u,i,o,g){
+    for(let i=0; i<arguments.length; i++){
+        document.write(arguments[i] + " :");
+    }
     
-// }
-// args(12,33,46,78)
+}
+args(12,33,46,78)
 
-// function printl(elements) {
-//     let result = ""; 
+function printl(elements) {
+    let result = ""; 
    
-//     for (let i = 1; i < arguments.length; i++) {
-//       result += arguments[i] + elements;
-//     }
-//     return result;
-//   }
-// document.writeln(printl(", ", "red", "orange", "blue"));
-// document.writeln(printl(", ", "red", "orange", "blue"));
-// document.writeln(printl(", ", "red", "orange", "blue"));
-// document.writeln(printl(", ", "red", "orange", "blue"));
+    for (let i = 1; i < arguments.length; i++) {
+      result += arguments[i] + elements;
+    }
+    return result;
+  }
+document.writeln(printl(", ", "red", "orange", "blue"));
+document.writeln(printl(", ", "red", "orange", "blue"));
+document.writeln(printl(", ", "red", "orange", "blue"));
+document.writeln(printl(", ", "red", "orange", "blue"));
 
 //Parameterss
 
 
 // default parametesrs
-// function multiply(a, b) {
-//     b = typeof b !== "undefined" ? b : 1;
-//     return a * b;
-//   }
+function multiply(a, b) {
+    b = typeof b !== "undefined" ? b : 1;
+    return a * b;
+  }
   
-// document.write(multiply(5)); 
+document.write(multiply(5)); 
 
 
-// function multiply(a, b=1) {
+function multiply(a, b=1) {
     
-//     return a * b;
-//   }
+    return a * b;
+  }
   
-// document.write(multiply(5)); 
+document.write(multiply(5)); 
 
 // rest parameter
 
-// function arry(a,...theArgs) {
-//     return a * theArgs;
-// }
-// const array= arry(12,13);
-// // document.write(array)
-// console.log(array)
+function arry(a,...theArgs) {
+    return a * theArgs;
+}
+const array= arry(12,13);
+// document.write(array)
+console.log(array)
 
 //arrow functions and this keyword
 
